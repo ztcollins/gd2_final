0519c5af0b98518d64aec4f59b070e3c84f8d69d
ctwaddell <ctwaddell@wisc.edu>
2024-03-16 17:26:13 -0500

    doc

--- Assets/Scripts/Handlers/SceneHandler.cs
+++ Assets/Scripts/Handlers/SceneHandler.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 
 public class SceneHandler : MonoBehaviour
 {
+    // TO USE:
+    // Add a CustomButton.cs script to a button object, and change the sceneToLoad field to the "SceneName" you want to load
+    // We do it this way to we can keep persistent objects (handlers) between scenes and the button can still call correct
+    // function on runtime without reference on compile time
     public string currentScene;
     void Awake()
     {
