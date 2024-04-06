3b3151de0a02bd77898bc4a5375a134d88998e8d
Zachary Collins <ztcollins02@gmail.com>
2024-04-06 15:13:31 -0500

    hub menu v1

--- Assets/Scripts/HubScripts/HubManager.cs
+++ Assets/Scripts/HubScripts/HubManager.cs
@@ -3,15 +3,21 @@ using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class HubManager : MonoBehaviour
 {
     private float money;
     private int day;
+    private Image selectedButton;
 
     #region References
         [SerializeField] TextMeshProUGUI moneyText;
         [SerializeField] TextMeshProUGUI dayText;
+        [SerializeField] Image newsButton;
+        [SerializeField] Image shopButton;
+        [SerializeField] GameObject newsPanel;
+        [SerializeField] GameObject shopPanel;
     #endregion
 
     public void Awake()
@@ -25,6 +31,53 @@ public class HubManager : MonoBehaviour
         day = GameObject.FindWithTag("StatsHandler").GetComponent<StatsHandler>().GetDay();
         SetMoney(money);
         SetDay(day);
+    }
+
+    public void NewsClicked()
+    {
+        newsPanel.SetActive(true);
+        shopPanel.SetActive(false);
+        UnselectButton();
+        SelectButton(newsButton);
+
+    }
+
+    public void ShopClicked()
+    {
+        shopPanel.SetActive(true);
+        newsPanel.SetActive(false);
+        UnselectButton();
+        SelectButton(shopButton);
+    }
+
+    public void SelectButton(Image buttonToSelect)
+    {
+        if(selectedButton != buttonToSelect)
+        {
+            buttonToSelect.color = Color.white;
+            selectedButton = buttonToSelect;
+        }
+        else
+        {
+            UnselectButton();
+            ClosePanels();
+            selectedButton = null;
+        }
+
+    }
+
+    public void UnselectButton()
+    {
+        if(selectedButton)
+        {
+            selectedButton.color = new Color32(89, 89, 93, 255);
+        }
+    }
+
+    public void ClosePanels()
+    {
+        shopPanel.SetActive(false);
+        newsPanel.SetActive(false);
     }
 
     public void SetDay(int value)
